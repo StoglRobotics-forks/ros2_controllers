78d79c2c6e03dce7ffa50fb7c7725f7cdb0854eb
GiridharBukka <giridhar6937@gmail.com>
2022-11-02 18:05:56 +0100

    created cont pkg and setup chained controller

---
+++ include/ros2_ackermann_cont/ackermann_steering_controller_ros2.hpp
@@ -0,0 +1,121 @@
+// Copyright (c) 2022, Stogl Robotics Consulting UG (haftungsbeschränkt) (template)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef ROS2_ACKERMANN_CONT__ACKERMANN_STEERING_CONTROLLER_ROS2_HPP_
+#define ROS2_ACKERMANN_CONT__ACKERMANN_STEERING_CONTROLLER_ROS2_HPP_
+
+#include <chrono>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "controller_interface/chainable_controller_interface.hpp"
+#include "ackermann_steering_controller_ros2_parameters.hpp"
+#include "ros2_ackermann_cont/visibility_control.h"
+#include "rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp"
+#include "rclcpp_lifecycle/state.hpp"
+#include "realtime_tools/realtime_buffer.h"
+#include "realtime_tools/realtime_publisher.h"
+#include "std_srvs/srv/set_bool.hpp"
+
+// TODO(anyone): Replace with controller specific messages
+#include "control_msgs/msg/joint_controller_state.hpp"
+#include "control_msgs/msg/joint_jog.hpp"
+
+namespace ros2_ackermann_cont
+{
+// name constants for state interfaces
+static constexpr size_t STATE_MY_ITFS = 0;
+
+// name constants for command interfaces
+static constexpr size_t CMD_MY_ITFS = 0;
+
+// TODO(anyone: example setup for control mode (usually you will use some enums defined in messages)
+enum class control_mode_type : std::uint8_t {
+  FAST = 0,
+  SLOW = 1,
+};
+
+class AckermannSteeringControllerRos2 : public controller_interface::ChainableControllerInterface
+{
+public:
+  TEMPLATES__ROS2_CONTROL__VISIBILITY_PUBLIC
+  AckermannSteeringControllerRos2();
+
+  TEMPLATES__ROS2_CONTROL__VISIBILITY_PUBLIC
+  controller_interface::CallbackReturn on_init() override;
+
+  TEMPLATES__ROS2_CONTROL__VISIBILITY_PUBLIC
+  controller_interface::InterfaceConfiguration command_interface_configuration() const override;
+
+  TEMPLATES__ROS2_CONTROL__VISIBILITY_PUBLIC
+  controller_interface::InterfaceConfiguration state_interface_configuration() const override;
+
+  TEMPLATES__ROS2_CONTROL__VISIBILITY_PUBLIC
+  controller_interface::CallbackReturn on_configure(
+    const rclcpp_lifecycle::State & previous_state) override;
+
+  TEMPLATES__ROS2_CONTROL__VISIBILITY_PUBLIC
+  controller_interface::CallbackReturn on_activate(
+    const rclcpp_lifecycle::State & previous_state) override;
+
+  TEMPLATES__ROS2_CONTROL__VISIBILITY_PUBLIC
+  controller_interface::CallbackReturn on_deactivate(
+    const rclcpp_lifecycle::State & previous_state) override;
+
+  TEMPLATES__ROS2_CONTROL__VISIBILITY_PUBLIC
+  controller_interface::return_type update_reference_from_subscribers() override;
+
+  TEMPLATES__ROS2_CONTROL__VISIBILITY_PUBLIC
+  controller_interface::return_type update_and_write_commands(
+    const rclcpp::Time & time, const rclcpp::Duration & period) override;
+
+  // TODO(anyone): replace the state and command message types
+  using ControllerReferenceMsg = control_msgs::msg::JointJog;
+  using ControllerModeSrvType = std_srvs::srv::SetBool;
+  using ControllerStateMsg = control_msgs::msg::JointControllerState;
+
+protected:
+  std::shared_ptr<ackermann_steering_controller_ros2::ParamListener> param_listener_;
+  ackermann_steering_controller_ros2::Params params_;
+
+  std::vector<std::string> state_joints_;
+
+  // Command subscribers and Controller State publisher
+  rclcpp::Subscription<ControllerReferenceMsg>::SharedPtr ref_subscriber_ = nullptr;
+  realtime_tools::RealtimeBuffer<std::shared_ptr<ControllerReferenceMsg>> input_ref_;
+  rclcpp::Duration ref_timeout_ = rclcpp::Duration::from_seconds(0.0);  // 0ms
+
+  rclcpp::Service<ControllerModeSrvType>::SharedPtr set_slow_control_mode_service_;
+  realtime_tools::RealtimeBuffer<control_mode_type> control_mode_;
+
+  using ControllerStatePublisher = realtime_tools::RealtimePublisher<ControllerStateMsg>;
+
+  rclcpp::Publisher<ControllerStateMsg>::SharedPtr s_publisher_;
+  std::unique_ptr<ControllerStatePublisher> state_publisher_;
+
+  // override methods from ChainableControllerInterface
+  std::vector<hardware_interface::CommandInterface> on_export_reference_interfaces() override;
+
+  bool on_set_chained_mode(bool chained_mode) override;
+
+private:
+  // callback for topic interface
+  TEMPLATES__ROS2_CONTROL__VISIBILITY_LOCAL
+  void reference_callback(const std::shared_ptr<ControllerReferenceMsg> msg);
+};
+
+}  // namespace ros2_ackermann_cont
+
+#endif  // ROS2_ACKERMANN_CONT__ACKERMANN_STEERING_CONTROLLER_ROS2_HPP_
---
+++ src/ackermann_steering_controller_ros2.cpp
@@ -0,0 +1,308 @@
+// Copyright (c) 2022, Stogl Robotics Consulting UG (haftungsbeschränkt) (template)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "ros2_ackermann_cont/ackermann_steering_controller_ros2.hpp"
+
+#include <limits>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "controller_interface/helpers.hpp"
+
+namespace
+{  // utility
+
+// TODO(destogl): remove this when merged upstream
+// Changed services history QoS to keep all so we don't lose any client service calls
+static constexpr rmw_qos_profile_t rmw_qos_profile_services_hist_keep_all = {
+  RMW_QOS_POLICY_HISTORY_KEEP_ALL,
+  1,  // message queue depth
+  RMW_QOS_POLICY_RELIABILITY_RELIABLE,
+  RMW_QOS_POLICY_DURABILITY_VOLATILE,
+  RMW_QOS_DEADLINE_DEFAULT,
+  RMW_QOS_LIFESPAN_DEFAULT,
+  RMW_QOS_POLICY_LIVELINESS_SYSTEM_DEFAULT,
+  RMW_QOS_LIVELINESS_LEASE_DURATION_DEFAULT,
+  false};
+
+using ControllerReferenceMsg = ros2_ackermann_cont::AckermannSteeringControllerRos2::ControllerReferenceMsg;
+
+// called from RT control loop
+void reset_controller_reference_msg(
+  const std::shared_ptr<ControllerReferenceMsg> & msg, const std::vector<std::string> & joint_names,
+  const std::shared_ptr<rclcpp_lifecycle::LifecycleNode> & node)
+{
+  msg->header.stamp = node->now();
+  msg->joint_names = joint_names;
+  msg->displacements.resize(joint_names.size(), std::numeric_limits<double>::quiet_NaN());
+  msg->velocities.resize(joint_names.size(), std::numeric_limits<double>::quiet_NaN());
+  msg->duration = std::numeric_limits<double>::quiet_NaN();
+}
+
+}  // namespace
+
+namespace ros2_ackermann_cont
+{
+AckermannSteeringControllerRos2::AckermannSteeringControllerRos2() : controller_interface::ChainableControllerInterface() {}
+
+controller_interface::CallbackReturn AckermannSteeringControllerRos2::on_init()
+{
+  control_mode_.initRT(control_mode_type::FAST);
+
+  try {
+    param_listener_ = std::make_shared<ackermann_steering_controller_ros2::ParamListener>(get_node());
+  } catch (const std::exception & e) {
+    fprintf(stderr, "Exception thrown during controller's init with message: %s \n", e.what());
+    return controller_interface::CallbackReturn::ERROR;
+  }
+
+  return controller_interface::CallbackReturn::SUCCESS;
+}
+
+controller_interface::CallbackReturn AckermannSteeringControllerRos2::on_configure(
+  const rclcpp_lifecycle::State & /*previous_state*/)
+{
+  params_ = param_listener_->get_params();
+
+  if (!params_.state_joints.empty()) {
+    state_joints_ = params_.state_joints;
+  } else {
+    state_joints_ = params_.joints;
+  }
+
+  if (params_.joints.size() != state_joints_.size()) {
+    RCLCPP_FATAL(
+      get_node()->get_logger(),
+      "Size of 'joints' (%d) and 'state_joints' (%d) parameters has to be the same!",
+      params_.joints.size(), state_joints_.size());
+    return CallbackReturn::FAILURE;
+  }
+
+  // topics QoS
+  auto subscribers_qos = rclcpp::SystemDefaultsQoS();
+  subscribers_qos.keep_last(1);
+  subscribers_qos.best_effort();
+
+  // Reference Subscriber
+  ref_timeout_ = rclcpp::Duration::from_seconds(params_.reference_timeout);
+  ref_subscriber_ = get_node()->create_subscription<ControllerReferenceMsg>(
+    "~/commands", subscribers_qos,
+    std::bind(&AckermannSteeringControllerRos2::reference_callback, this, std::placeholders::_1));
+
+  std::shared_ptr<ControllerReferenceMsg> msg = std::make_shared<ControllerReferenceMsg>();
+  reset_controller_reference_msg(msg, params_.joints, get_node());
+  input_ref_.writeFromNonRT(msg);
+
+  auto set_slow_mode_service_callback =
+    [&](
+      const std::shared_ptr<ControllerModeSrvType::Request> request,
+      std::shared_ptr<ControllerModeSrvType::Response> response) {
+      if (request->data) {
+        control_mode_.writeFromNonRT(control_mode_type::SLOW);
+      } else {
+        control_mode_.writeFromNonRT(control_mode_type::FAST);
+      }
+      response->success = true;
+    };
+
+  set_slow_control_mode_service_ = get_node()->create_service<ControllerModeSrvType>(
+    "~/set_slow_control_mode", set_slow_mode_service_callback,
+    rmw_qos_profile_services_hist_keep_all);
+
+  try {
+    // State publisher
+    s_publisher_ =
+      get_node()->create_publisher<ControllerStateMsg>("~/state", rclcpp::SystemDefaultsQoS());
+    state_publisher_ = std::make_unique<ControllerStatePublisher>(s_publisher_);
+  } catch (const std::exception & e) {
+    fprintf(
+      stderr, "Exception thrown during publisher creation at configure stage with message : %s \n",
+      e.what());
+    return controller_interface::CallbackReturn::ERROR;
+  }
+
+  // TODO(anyone): Reserve memory in state publisher depending on the message type
+  state_publisher_->lock();
+  state_publisher_->msg_.header.frame_id = params_.joints[0];
+  state_publisher_->unlock();
+
+  RCLCPP_INFO(get_node()->get_logger(), "configure successful");
+  return controller_interface::CallbackReturn::SUCCESS;
+}
+
+void AckermannSteeringControllerRos2::reference_callback(const std::shared_ptr<ControllerReferenceMsg> msg)
+{
+  const auto age_of_last_command = get_node()->now() - msg->header.stamp;
+  // if no timestamp provided use current time for command timestamp
+  if (msg->header.stamp.sec == 0 && msg->header.stamp.nanosec == 0u) {
+    RCLCPP_WARN(
+      get_node()->get_logger(),
+      "Timestamp in header is missing, using current time as command timestamp.");
+    msg->header.stamp = get_node()->now();
+  }
+  if (msg->joint_names.size() == params_.joints.size()) {
+    if (ref_timeout_ == rclcpp::Duration::from_seconds(0) || age_of_last_command <= ref_timeout_) {
+    input_ref_.writeFromNonRT(msg);
+  } else {
+    RCLCPP_ERROR(
+      get_node()->get_logger(),
+        "Received message has timestamp %.10f older then allowed timeout at timestamp %.10f",
+         rclcpp::Time(msg->header.stamp).seconds(), get_node()->now().seconds());
+    }
+  } else {
+    RCLCPP_ERROR(
+      get_node()->get_logger(),
+      "Received %zu , but expected %zu joints in command. Ignoring message.",
+      msg->joint_names.size(), params_.joints.size());
+        
+  }
+}
+
+controller_interface::InterfaceConfiguration AckermannSteeringControllerRos2::command_interface_configuration() const
+{
+  controller_interface::InterfaceConfiguration command_interfaces_config;
+  command_interfaces_config.type = controller_interface::interface_configuration_type::INDIVIDUAL;
+
+  command_interfaces_config.names.reserve(params_.joints.size());
+  for (const auto & joint : params_.joints) {
+    command_interfaces_config.names.push_back(joint + "/" + params_.interface_name);
+  }
+
+  return command_interfaces_config;
+}
+
+controller_interface::InterfaceConfiguration AckermannSteeringControllerRos2::state_interface_configuration() const
+{
+  controller_interface::InterfaceConfiguration state_interfaces_config;
+  state_interfaces_config.type = controller_interface::interface_configuration_type::INDIVIDUAL;
+
+  state_interfaces_config.names.reserve(state_joints_.size());
+  for (const auto & joint : state_joints_) {
+    state_interfaces_config.names.push_back(joint + "/" + params_.interface_name);
+  }
+
+  return state_interfaces_config;
+}
+
+
+
+std::vector<hardware_interface::CommandInterface> AckermannSteeringControllerRos2::on_export_reference_interfaces()
+{
+  reference_interfaces_.resize(state_joints_.size(), std::numeric_limits<double>::quiet_NaN());
+
+  std::vector<hardware_interface::CommandInterface> reference_interfaces;
+  reference_interfaces.reserve(reference_interfaces_.size());
+
+  for (size_t i = 0; i < reference_interfaces_.size(); ++i) {
+    reference_interfaces.push_back(hardware_interface::CommandInterface(
+      get_node()->get_name(), state_joints_[i] + "/" + params_.interface_name,
+      &reference_interfaces_[i]));
+  }
+
+  return reference_interfaces;
+}
+
+bool AckermannSteeringControllerRos2::on_set_chained_mode(bool chained_mode)
+{
+  // Always accept switch to/from chained mode
+  return true || chained_mode;
+}
+
+controller_interface::CallbackReturn AckermannSteeringControllerRos2::on_activate(
+  const rclcpp_lifecycle::State & /*previous_state*/)
+{
+  // Set default value in command
+  reset_controller_reference_msg(*(input_ref_.readFromRT()), state_joints_, get_node());
+
+  return controller_interface::CallbackReturn::SUCCESS;
+}
+
+controller_interface::CallbackReturn AckermannSteeringControllerRos2::on_deactivate(
+  const rclcpp_lifecycle::State & /*previous_state*/)
+{
+  // TODO(anyone): depending on number of interfaces, use definitions, e.g., `CMD_MY_ITFS`,
+  // instead of a loop
+  for (size_t i = 0; i < command_interfaces_.size(); ++i) {
+    command_interfaces_[i].set_value(std::numeric_limits<double>::quiet_NaN());
+  }
+  return controller_interface::CallbackReturn::SUCCESS;
+}
+
+controller_interface::return_type AckermannSteeringControllerRos2::update_reference_from_subscribers()
+{
+  auto current_ref = input_ref_.readFromRT();
+  const auto age_of_last_command = get_node()->now() - (*current_ref)->header.stamp;
+
+  // TODO(anyone): depending on number of interfaces, use definitions, e.g., `CMD_MY_ITFS`,
+  // instead of a loop
+  for (size_t i = 0; i < reference_interfaces_.size(); ++i) {
+    // send message only if there is no timeout
+    if (age_of_last_command <= ref_timeout_ || ref_timeout_ == rclcpp::Duration::from_seconds(0)) {
+      if (!std::isnan((*current_ref)->displacements[i])) {
+        if (*(control_mode_.readFromRT()) == control_mode_type::SLOW) {
+          (*current_ref)->displacements[i] /= 2;
+        }
+        reference_interfaces_[i] = (*current_ref)->displacements[i];
+        if (ref_timeout_ == rclcpp::Duration::from_seconds(0)){
+          (*current_ref)->displacements[i] = std::numeric_limits<double>::quiet_NaN();
+        }
+      }
+    } else {
+      (*current_ref)->displacements[i] = std::numeric_limits<double>::quiet_NaN();
+    }
+  }
+  return controller_interface::return_type::OK;
+}
+
+controller_interface::return_type AckermannSteeringControllerRos2::update_and_write_commands(
+  const rclcpp::Time & time, const rclcpp::Duration & /*period*/)
+{
+  auto current_ref = input_ref_.readFromRT();
+  const auto age_of_last_command = get_node()->now() - (*current_ref)->header.stamp;
+
+  // TODO(anyone): depending on number of interfaces, use definitions, e.g., `CMD_MY_ITFS`,
+  // instead of a loop
+  for (size_t i = 0; i < command_interfaces_.size(); ++i) {
+    // send message only if there is no timeout
+    if (age_of_last_command <= ref_timeout_ || ref_timeout_ == rclcpp::Duration::from_seconds(0)) {
+      if (!std::isnan(reference_interfaces_[i])) {
+        if (*(control_mode_.readFromRT()) == control_mode_type::SLOW) {
+          reference_interfaces_[i] /= 2;
+        }
+        command_interfaces_[i].set_value(reference_interfaces_[i]);
+        if (ref_timeout_ == rclcpp::Duration::from_seconds(0)){
+          reference_interfaces_[i] = std::numeric_limits<double>::quiet_NaN();
+        }
+      }
+    } else {
+      reference_interfaces_[i] = std::numeric_limits<double>::quiet_NaN();
+    }
+  }
+
+  if (state_publisher_ && state_publisher_->trylock()) {
+    state_publisher_->msg_.header.stamp = time;
+    state_publisher_->msg_.set_point = command_interfaces_[CMD_MY_ITFS].get_value();
+    state_publisher_->unlockAndPublish();
+  }
+
+  return controller_interface::return_type::OK;
+}
+
+}  // namespace ros2_ackermann_cont
+
+#include "pluginlib/class_list_macros.hpp"
+
+PLUGINLIB_EXPORT_CLASS(
+  ros2_ackermann_cont::AckermannSteeringControllerRos2, controller_interface::ChainableControllerInterface)
