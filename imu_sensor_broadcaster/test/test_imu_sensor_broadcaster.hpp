822dcbeced95df903daeae5e3ab023935db16b06
Manuel M <mamueluth@gmail.com>
2022-11-21 15:00:41 +0100

    adapt to changes made to loandedInterface

--- imu_sensor_broadcaster/test/test_imu_sensor_broadcaster.hpp
+++ imu_sensor_broadcaster/test/test_imu_sensor_broadcaster.hpp
@@ -55,26 +55,36 @@ protected:
   const std::string sensor_name_ = "imu_sensor";
   const std::string frame_id_ = "imu_sensor_frame";
   std::array<double, 10> sensor_values_ = {1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9, 10.10};
-  hardware_interface::StateInterface imu_orientation_x_{
-    sensor_name_, "orientation.x", &sensor_values_[0]};
-  hardware_interface::StateInterface imu_orientation_y_{
-    sensor_name_, "orientation.y", &sensor_values_[1]};
-  hardware_interface::StateInterface imu_orientation_z_{
-    sensor_name_, "orientation.z", &sensor_values_[2]};
-  hardware_interface::StateInterface imu_orientation_w_{
-    sensor_name_, "orientation.w", &sensor_values_[3]};
-  hardware_interface::StateInterface imu_angular_velocity_x_{
-    sensor_name_, "angular_velocity.x", &sensor_values_[4]};
-  hardware_interface::StateInterface imu_angular_velocity_y_{
-    sensor_name_, "angular_velocity.y", &sensor_values_[5]};
-  hardware_interface::StateInterface imu_angular_velocity_z_{
-    sensor_name_, "angular_velocity.z", &sensor_values_[6]};
-  hardware_interface::StateInterface imu_linear_acceleration_x_{
-    sensor_name_, "linear_acceleration.x", &sensor_values_[7]};
-  hardware_interface::StateInterface imu_linear_acceleration_y_{
-    sensor_name_, "linear_acceleration.y", &sensor_values_[8]};
-  hardware_interface::StateInterface imu_linear_acceleration_z_{
-    sensor_name_, "linear_acceleration.z", &sensor_values_[9]};
+  std::shared_ptr<hardware_interface::StateInterface> imu_orientation_x_ =
+    std::make_shared<hardware_interface::StateInterface>(
+      sensor_name_, "orientation.x", &sensor_values_[0]);
+  std::shared_ptr<hardware_interface::StateInterface> imu_orientation_y_ =
+    std::make_shared<hardware_interface::StateInterface>(
+      sensor_name_, "orientation.y", &sensor_values_[1]);
+  std::shared_ptr<hardware_interface::StateInterface> imu_orientation_z_ =
+    std::make_shared<hardware_interface::StateInterface>(
+      sensor_name_, "orientation.z", &sensor_values_[2]);
+  std::shared_ptr<hardware_interface::StateInterface> imu_orientation_w_ =
+    std::make_shared<hardware_interface::StateInterface>(
+      sensor_name_, "orientation.w", &sensor_values_[3]);
+  std::shared_ptr<hardware_interface::StateInterface> imu_angular_velocity_x_ =
+    std::make_shared<hardware_interface::StateInterface>(
+      sensor_name_, "angular_velocity.x", &sensor_values_[4]);
+  std::shared_ptr<hardware_interface::StateInterface> imu_angular_velocity_y_ =
+    std::make_shared<hardware_interface::StateInterface>(
+      sensor_name_, "angular_velocity.y", &sensor_values_[5]);
+  std::shared_ptr<hardware_interface::StateInterface> imu_angular_velocity_z_ =
+    std::make_shared<hardware_interface::StateInterface>(
+      sensor_name_, "angular_velocity.z", &sensor_values_[6]);
+  std::shared_ptr<hardware_interface::StateInterface> imu_linear_acceleration_x_ =
+    std::make_shared<hardware_interface::StateInterface>(
+      sensor_name_, "linear_acceleration.x", &sensor_values_[7]);
+  std::shared_ptr<hardware_interface::StateInterface> imu_linear_acceleration_y_ =
+    std::make_shared<hardware_interface::StateInterface>(
+      sensor_name_, "linear_acceleration.y", &sensor_values_[8]);
+  std::shared_ptr<hardware_interface::StateInterface> imu_linear_acceleration_z_ =
+    std::make_shared<hardware_interface::StateInterface>(
+      sensor_name_, "linear_acceleration.z", &sensor_values_[9]);
 
   std::unique_ptr<FriendIMUSensorBroadcaster> imu_broadcaster_;
 
