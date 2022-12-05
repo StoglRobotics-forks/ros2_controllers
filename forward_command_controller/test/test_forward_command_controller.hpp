1cc40be1bbf362034d1de205a38fbde057f4f8ca
Manuel M <mamueluth@gmail.com>
2022-12-05 10:56:41 +0100

    adjust tests to reflect changes in handles

--- forward_command_controller/test/test_forward_command_controller.hpp
+++ forward_command_controller/test/test_forward_command_controller.hpp
@@ -66,9 +66,12 @@ protected:
   const std::vector<std::string> joint_names_ = {"joint1", "joint2", "joint3"};
   std::vector<double> joint_commands_ = {1.1, 2.1, 3.1};
 
-  CommandInterface joint_1_pos_cmd_{joint_names_[0], HW_IF_POSITION, &joint_commands_[0]};
-  CommandInterface joint_2_pos_cmd_{joint_names_[1], HW_IF_POSITION, &joint_commands_[1]};
-  CommandInterface joint_3_pos_cmd_{joint_names_[2], HW_IF_POSITION, &joint_commands_[2]};
+  std::shared_ptr<CommandInterface> joint_1_pos_cmd_ =
+    std::make_shared<CommandInterface>(joint_names_[0], HW_IF_POSITION, &joint_commands_[0]);
+  std::shared_ptr<CommandInterface> joint_2_pos_cmd_ =
+    std::make_shared<CommandInterface>(joint_names_[1], HW_IF_POSITION, &joint_commands_[1]);
+  std::shared_ptr<CommandInterface> joint_3_pos_cmd_ =
+    std::make_shared<CommandInterface>(joint_names_[2], HW_IF_POSITION, &joint_commands_[2]);
 };
 
 #endif  // TEST_FORWARD_COMMAND_CONTROLLER_HPP_
