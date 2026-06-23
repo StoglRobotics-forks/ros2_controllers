feb5140c3e9c29f9e8ba60663ecdc0b6bc6a1bbc
Hajar Bartakh <bartakh.hajar@gmail.com>
2026-06-23 15:30:15 +0100

    Added parameter to enable reconfigure while in engaged state

--- gpio_controllers/src/gpio_tool_controller.cpp
+++ gpio_controllers/src/gpio_tool_controller.cpp
@@ -999,13 +999,13 @@ GpioToolController::EngagingSrvType::Response GpioToolController::process_reconf
                        "' action.Please wait until it finishes.";
   }
   // This is OK to access `current_state_` as we are in the IDLE state and it is not being modified
-  if (response.success && current_state_.get() != params_.disengaged.name)
+  if (response.success && !params_.enable_config_engaged && current_state_.get() != params_.disengaged.name)
   {
     response.success = false;
     response.message = "Tool can be reconfigured only in '" + params_.disengaged.name +
                        "' state. Current state is '" + current_state_.get() + "'.";
   }
-  if (response.success)
+  if (response.success || params_.enable_config_engaged)
   {
     current_tool_action_.store(ToolAction::RECONFIGURING);
     current_tool_transition_.store(GPIOToolTransition::SET_BEFORE_COMMAND);
